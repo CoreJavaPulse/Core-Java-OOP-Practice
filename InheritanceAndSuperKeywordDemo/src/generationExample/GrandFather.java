4c0ff70fbb123b9ebca36f4970a425e14ea794b3
Hitesh Mane <HiteshMane5hm@gmail.com>
2025-12-20 13:57:48 +0530

    InheritanceAndSuperKeywordDemo Project added.

---
+++ InheritanceAndSuperKeywordDemo/src/generationExample/GrandFather.java
@@ -0,0 +1,25 @@
+package generationExample;
+
+public class GrandFather {
+	
+	private int gage;
+
+	public GrandFather(int gage) {
+		super();
+		this.gage = gage;
+	}
+
+	public int getGage() {
+		return gage;
+	}
+
+	public void setGage(int gage) {
+		this.gage = gage;
+	}
+	
+	public void showAge()
+	{
+		System.out.println("GrandFather Age is :"+gage);
+	}
+
+}
---
+++ InheritanceAndSuperKeywordDemo/src/generationExample/MainClass.java
@@ -0,0 +1,12 @@
+package generationExample;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		
+		Son sobj = new Son(20);
+		sobj.showAge();
+
+	}
+
+}
