e73bc35f7ba398b3b1d049633c30d7b0f6f3c32d
Hitesh Mane <HiteshMane5hm@gmail.com>
2025-12-21 13:46:11 +0530

    ExceptionHandlingDemo Project added

---
+++ ExceptionHandlingDemo/src/InvalidAgeException.java
@@ -0,0 +1,9 @@
+
+public class InvalidAgeException  extends Exception{
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "Invalid Age : Should Be  age > 18";
+	}
+}
