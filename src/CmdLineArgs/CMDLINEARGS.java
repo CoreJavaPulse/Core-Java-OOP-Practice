cce798e7dd6860100e4163fe58fd12ce988d3472
Hitesh Mane <HiteshMane5hm@gmail.com>
2026-01-06 17:32:49 +0530

    Command Line Arguments example added.

---
+++ src/CmdLineArgs/CMDLINEARGS.java
@@ -0,0 +1,16 @@
+package CmdLineArgs;
+
+public class CMDLINEARGS {
+
+	public static void main(String[] args) {
+		
+		String s1  = args[0];
+		String s2  = args[1];
+		int n1 = Integer.parseInt(s1);
+		int n2 = Integer.parseInt(s2);
+		int add =n1+n2;
+		System.out.println("Addition Is :"+add);
+
+	}
+
+}
