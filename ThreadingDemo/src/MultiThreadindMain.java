77f6295e113ce3a07ab0f668440537c334fa12b2
Hitesh Mane <HiteshMane5hm@gmail.com>
2026-01-10 17:03:43 +0530

    Multithreading Example added.

---
+++ ThreadingDemo/src/MultiThreadindMain.java
@@ -0,0 +1,16 @@
+
+public class MultiThreadindMain {
+
+	public static void main(String[] args) {
+		
+		MultiThreadingExample example1 = new MultiThreadingExample("Hitesh");
+		MultiThreadingExample example2 = new MultiThreadingExample("Deepali");
+		example1.start();
+		example2.start();
+		example1.setName("Thread 1");
+		example2.setName("Thread 2");
+		System.out.println("Thread 1 Name :"+example1.getName());
+		System.out.println("Thank You.");
+		
+	}
+}
---
+++ ThreadingDemo/src/MultiThreadingExample.java
@@ -0,0 +1,18 @@
+
+public class MultiThreadingExample extends Thread{
+	
+	public String str;
+
+	public MultiThreadingExample(String str) {
+		super();
+		this.str = str;
+	}
+
+	@Override
+	public void run() {
+		for(int i = 1;i<=5;i++)
+		{
+			System.out.println(str+"\t"+i);
+		}
+	}
+}
