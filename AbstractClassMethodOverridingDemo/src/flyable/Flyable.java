07d89d8c0acfe9c9b2c8353d6db2093b23954b27
Hitesh Mane <HiteshMane5hm@gmail.com>
2026-01-27 12:44:16 +0530

    AbstractClassMethodOverridingDemo project added.

---
+++ AbstractClassMethodOverridingDemo/src/flyable/Flyable.java
@@ -0,0 +1,9 @@
+package flyable;
+
+public abstract class Flyable {
+
+	public abstract void fly();
+	public void sayThanks() {
+		System.out.println("Say Thanks");
+	}
+}
---
+++ AbstractClassMethodOverridingDemo/src/flyable/Helicopter.java
@@ -0,0 +1,11 @@
+package flyable;
+
+public class Helicopter extends Flyable {
+
+	@Override
+	public void fly() {
+		System.out.println("Helicopter can Fly");
+		
+	}
+
+}
