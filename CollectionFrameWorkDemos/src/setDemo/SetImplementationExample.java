7cd36d49069c4300431d8f04de872025b396fc87
Hitesh Mane <HiteshMane5hm@gmail.com>
2026-03-10 17:26:18 +0530

    Collection: Set Implementation Added.

---
+++ CollectionFrameWorkDemos/src/setDemo/SetImplementationExample.java
@@ -0,0 +1,31 @@
+package setDemo;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class SetImplementationExample {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Tree Set Implementation");
+		Set<Integer> treeset = new TreeSet<Integer>();
+
+		treeset.add(10);
+		treeset.add(110);
+		treeset.add(132);
+		treeset.add(14);
+		treeset.add(10);
+		System.out.println(treeset);
+		
+		System.out.println("Hash Set Implementation");
+		Set<Integer> hashset = new HashSet<Integer>();
+		
+		hashset.add(10);
+		hashset.add(110);
+		hashset.add(132);
+		hashset.add(14);
+		hashset.add(10);
+		System.out.println(hashset);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
+	}
+}
