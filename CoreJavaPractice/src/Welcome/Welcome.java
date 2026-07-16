90da9b2e0fba061f0864bf61964f3c818a1302ca
Hitesh Mane <Hiteshmane5hm@gmail.com>
2026-07-16 18:50:25 +0530

    Test SSH

--- CoreJavaPractice/src/Welcome/Welcome.java
+++ CoreJavaPractice/src/Welcome/Welcome.java
@@ -5,7 +5,7 @@ public class Welcome {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome To Java!!!");
-		System.out.println("Here We Go");
+		System.out.println("Here We Go Ubuntu");
 	}
 
 }
